2a9745d49b3575909bf79a7a378e126435aadd19
kani-s-h-k <kanishk.malik.dev@gmail.com>
2026-06-14 14:31:37 +0530

    added update,delete operations

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,28 @@
 #include <bits/stdc++.h>
 #include <fstream>
 #include <conio.h>
+#include <windows.h>
 #include <iomanip>
 using namespace std;
 
+bool checkRoll(string target){
+	ifstream fin("students.txt");
+
+	string line;
+
+	while(getline(fin,line)){
+		stringstream ss(line);
+		string roll;
+		getline(ss,roll,',');
+		if(roll == target){ 
+			return true;
+		}
+	}
+	return false;
+}
+
 int main(){
+
 
 // #ifndef ONLINE_JUDGE
 // 	freopen("E:/sublime/input.txt","r",stdin);
@@ -14,6 +32,7 @@ int main(){
 	int ch;
 	bool flag = true;
 	do{
+		cout<<endl;
 		cout << "****************************************\n";
 		cout << "*      STUDENT MANAGEMENT SYSTEM       *\n";
 		cout << "****************************************\n";
@@ -31,35 +50,34 @@ int main(){
 		switch(ch){
 			case 1: 
 			{
-				ofstream fout("students.txt",ios::app);
-
-				if (!fout) {
-    				cout << "\nError opening file!\n";
-    				break;
-				}
-
-
 				string name, roll;
 				string marks;
 
 				cout<<"Enter Roll Number: ";
 				cin>>roll;
 
-				cout<<"\nEnter Name: ";
-				cin.ignore(numeric_limits<streamsize>::max(), '\n');
-				getline(cin,name);
+				if(!checkRoll(roll)){
+					ofstream fout("students.txt",ios::app);
 
-				cout<<"\nEnter Marks: ";
-				cin>>marks;
-				fout<<roll<<","<<name<<","<<marks<<"\n";
-				cout<<"\nStudent Added Successfully!";
-				cout<<endl;
+					if (!fout) {
+	    				cout << "\nError opening file!\n";
+	    				break;
+					}
+					cout<<"\nEnter Name: ";
+					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					getline(cin,name);
 
-				cout<<"\nPlease Enter to continue...";
-
-				while(_getch()!=13){} //Waiting while the user enters the specified key...
-
-				fout.close();
+					cout<<"\nEnter Marks: ";
+					cin>>marks;
+					fout<<roll<<","<<name<<","<<marks<<"\n";
+					cout<<"\nStudent Added Successfully!";
+					cout<<endl;
+					fout.close();
+					cout<<"\nPlease Enter to continue...";
+					while(_getch()!=13){} //Waiting while the user enters the specified key...
+				} else{
+					cout<<"\nStudent already registered!";
+				}
 				cout<<endl;
 				break;
 			}
@@ -89,12 +107,9 @@ int main(){
 					maxName = max(maxName, (int)name.size());
 					maxMarks = max(maxMarks, (int)marks.size());
 				}
-				fin.close();
-				fin.open("students.txt");
-				if(!fin){
-				    cout << "Error reopening file!\n";
-				    break;
- 				}
+				fin.clear();
+				fin.seekg(0);
+
 				cout << "\n--- View All Students ---\n\n";
 
 				cout
@@ -106,7 +121,7 @@ int main(){
 						<< setw(maxMarks+2)<< "Marks"
 						<< endl;
 
-				cout << "-----------+---------------+--------\n";
+				cout << string(maxRoll + maxName + maxMarks + 10, '-') << '\n';
 				
 				string line2;
 
@@ -126,21 +141,159 @@ int main(){
 					     << setw(maxMarks+2) << marks
 					     << endl;
 				}
-
+				fin.close();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
 				cout<<"\nPlease Enter to continue...";
 
 				while(_getch()!=13){} //Waiting while the user enters the specified key...
-
-				fin.close();
 				cout<<endl;
 				break;
 			}
-			case 3:
+			case 3:{
+				cout << "\n--- Search Student ---\n\n";
+				bool fl = false;
+				string target;
+				cout<<"Enter Roll Number: ";
+				cin>>target;
+				cout<<endl;
+				ifstream fin("students.txt");
+				string roll,name,marks;
+				string line;
+				while(getline(fin,line)){
+					stringstream ss(line);
+					getline(ss,roll,',');
+					if(roll == target){
+						fl = true;
+						getline(ss,name,',');
+						getline(ss,marks);
+
+						int maxRoll = max((int)roll.size(),8);
+						int maxName = max((int)name.size(),4);
+						int maxMarks = max((int)marks.size(),5);
+
+						cout
+						<< left
+						<< setw(maxRoll+2) << "Roll No."
+						<<"| "
+						<< setw(maxName+2) << "Name"
+						<<"| "
+						<< setw(maxMarks+2)<< "Marks" 
+						<< endl;
+
+						cout
+						<< left
+						<< setw(maxRoll+2) << roll
+						<<"| "
+						<< setw(maxName+2) << name
+						<<"| "
+						<< setw(maxMarks+2)<< marks
+						<< endl;	
+						break;
+					}
+				}
+				fin.close();
+				if(!fl) cout<<"Student not registered!"<<endl;
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				cout<<"\nPlease Enter to continue...";
+				while(_getch()!=13){} //Waiting while the user enters the specified key...
 				break;
-			case 4:
+			}
+			case 4: 
+			{
+				cout << "\n--- Update Student Details ---\n\n";
+				string roll,Newname,Newmarks;
+				cout<<"\n Enter Roll Number: ";
+				cin>>roll;
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+				if(checkRoll(roll))
+				{
+					cout<<"\n Enter new name: ";
+					getline(cin,Newname);
+					cout<<"\nEnter new marks: ";
+					cin>>Newmarks;				
+
+					ifstream fin("students.txt");
+					ofstream fout("temp.txt");
+
+					string line;
+					while(getline(fin,line)){
+						stringstream ss(line);
+						string name,marks,r;
+						getline(ss,r,',');
+						getline(ss,name,',');
+						getline(ss,marks);
+
+						if(r!=roll){
+							fout<<line<<endl;
+						}
+						else{
+							fout<<roll<<","<<Newname<<","<<Newmarks<<"\n";
+						}
+
+					}
+						fin.close();
+						fout.close();
+						remove("students.txt");
+						if(rename("temp.txt","students.txt")==0){
+							cout<<"\nSuccesfully Updated!"<<endl;
+						}
+						else{
+							cout << "\nError Updating Student!\n";
+						}
+				}
+				else cout<<"Student Not Registered!"<<endl;
+				
+				
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				cout<<"\nPlease Enter to continue...";
+				while(_getch()!=13){} //Waiting while the user enters the specified key...
 				break;
+			}
 			case 5:
+				{
+				cout << "\n--- Delete Student Registration---\n\n";
+				string roll,Newname,Newmarks;
+				cout<<"\n Enter Roll Number: ";
+				cin>>roll;
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+				if(checkRoll(roll))
+				{	
+
+					ifstream fin("students.txt");
+					ofstream fout("temp.txt");
+
+					string line;
+					while(getline(fin,line)){
+						stringstream ss(line);
+						string r;
+						getline(ss,r,',');
+						
+
+						if(r!=roll){
+							fout<<line<<endl;
+						}
+
+					}
+						fin.close();
+						fout.close();
+						remove("students.txt");
+						if(rename("temp.txt","students.txt")==0){
+							cout<<"\nSuccesfully Deleted!"<<endl;
+						}
+						else{
+							cout << "\nError Deleting Student Registration!\n";
+						}
+				}
+				else cout<<"Student Not Registered!"<<endl;
+				
+				
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				cout<<"\nPlease Enter to continue...";
+				while(_getch()!=13){} //Waiting while the user enters the specified key...
 				break;
+			}
 			case 6:
 				break;
 			case 7:
